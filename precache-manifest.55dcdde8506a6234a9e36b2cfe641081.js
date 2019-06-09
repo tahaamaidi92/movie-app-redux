50997781b0ded4c29f81dcba1faf9a9fd9fdd1f4
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-06-10 00:42:17 +0100

    Updates

---
+++ precache-manifest.55dcdde8506a6234a9e36b2cfe641081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e25cd64d4601fbafeccbe415d0d1763",
+    "url": "/movie-app-redux/index.html"
+  },
+  {
+    "revision": "6e7aa534dcd50626e5c8",
+    "url": "/movie-app-redux/static/css/main.7d15e16b.chunk.css"
+  },
+  {
+    "revision": "a5eb1d241c5b4a674a36",
+    "url": "/movie-app-redux/static/js/2.ac4c067d.chunk.js"
+  },
+  {
+    "revision": "6e7aa534dcd50626e5c8",
+    "url": "/movie-app-redux/static/js/main.01afb866.chunk.js"
+  },
+  {
+    "revision": "4d16e11505f4767a8025",
+    "url": "/movie-app-redux/static/js/runtime~main.1d159a58.js"
+  }
+]);
